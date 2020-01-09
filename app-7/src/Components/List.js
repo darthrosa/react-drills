c6d42cff330483943a2069164c7b95f87ad5d2de
Nick Rosa <nicholascastrorosa@gmail.com>
2020-01-09 09:04:08 -0700

    Added components(not done

---
+++ app-7/src/Components/List.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Todo from './Todo'
+
+class List extends Component {
+    render(){
+        return(
+            <div>
+                <Todo />
+                List
+            </div>
+        )
+    }
+}
+
+export default List;
---
+++ app-7/src/Components/NewTask.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NewTask extends Component {
+    render(){
+        return(
+            <div>
+                NewTask
+            </div>
+        )
+    }
+}
+
+export default NewTask;
